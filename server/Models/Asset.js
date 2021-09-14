9d9b2aaccdbd623b8c8b5d34a3d45bac538e0a5e
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-09-14 16:54:35 -0400

    create models

---
+++ server/Models/Asset.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const assetSchema = new Schema(
+    {
+        description: {
+            type: String,
+            required: true
+        },
+        bookValue: {
+            type: Number,
+            required: true
+        },
+        monthPurchased: {
+            type: Date,
+            required: true
+        },
+        usefulLife: {
+            type: Number,
+            required: true,
+            enum: [3,5]
+        },
+        depreciationMethod: {
+            type: String,
+            required: true,
+            enum: ['Straight Line'],
+            default: 'Straight Line'
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }    
+)
+
+
+const Asset = mongoose.model('Asset', assetSchema);
+
+module.exports = Asset;
