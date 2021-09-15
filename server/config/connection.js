6dfb5a1839a94712cc8ecb913ec69114b1314757
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-09-15 12:11:33 -0400

    connect config file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Subsidiary-Ledger', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
