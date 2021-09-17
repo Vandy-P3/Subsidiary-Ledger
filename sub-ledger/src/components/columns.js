296b74c8d74f390b8d40b473afc41e5141cc00b0
michaelab24 <michaelbarnes459@yahoo.com>
2021-09-16 22:48:48 -0500

    table looks even better, makes more sense

--- sub-ledger/src/components/columns.js
+++ sub-ledger/src/components/columns.js
@@ -1,30 +1,30 @@
-export const COLUMNS = [
-    {
-        Header: 'Asset',
-        accessor: 'name'
-    },
-    { 
-        Header: 'Book Value',
-        accessor: 'bookValue'
-    },
-    {
-        Header: 'Month Purchased',
-        accessor: 'monthPurchased'
-    },
-    {
-        Header: 'Useful Life',
-        accessor: 'usefulLife'
-    },
-    { 
-        Header: 'Depreciation Method',
-        accessor: 'depreciationMethod'
-    },
-    { 
-        Header: 'Monthly Depreciation Expense',
-        accessor: 'monthlyDepreciationExpense'
-    },
-    {
-        Header: 'Accumulated Depreciation',
-        accessor: 'accumulatedDepreciation'
-    }
-]
+// export const COLUMNS = [
+//     {
+//         Header: 'Asset',
+//         accessor: 'name'
+//     },
+//     { 
+//         Header: 'Book Value',
+//         accessor: 'bookValue'
+//     },
+//     {
+//         Header: 'Month Purchased',
+//         accessor: 'monthPurchased'
+//     },
+//     {
+//         Header: 'Useful Life',
+//         accessor: 'usefulLife'
+//     },
+//     { 
+//         Header: 'Depreciation Method',
+//         accessor: 'depreciationMethod'
+//     },
+//     { 
+//         Header: 'Monthly Depreciation Expense',
+//         accessor: 'monthlyDepreciationExpense'
+//     },
+//     {
+//         Header: 'Accumulated Depreciation',
+//         accessor: 'accumulatedDepreciation'
+//     }
+// ]
