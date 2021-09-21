035803a2ee17b3f69c57ebd9647e2e3781270a33
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-09-21 16:06:29 -0400

    create navbar component

--- sub-ledger/src/App.js
+++ sub-ledger/src/App.js
@@ -1,71 +1,30 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-// import Auth from "./utils/auth";
-// import Login from "./components/Login/Login";
-// import SignUp from "./components/Signup/Signup";
-// import HomePage from "./components/HomePage/HomePage";
-// import AssetForm from "./components/AssetForm/AssetForm";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Login from "./components/Login/Login";
+import SignUp from "./components/Signup/Signup";
+import HomePage from "./components/HomePage/HomePage";
+import AssetForm from "./components/AssetForm/AssetForm";
 
 function App() {
   return (
-    <h1>Here</h1>
-
-
-    // <Router>
-    //   <div className="App">
-    //     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-    //       <div className="container">
-    //         <Link className="navbar-brand" to={"/"}>
-    //           Depreciation Ledger
-    //         </Link>
-    //         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-    //           <ul className="navbar-nav ml-auto">
-    //             {Auth.loggedIn() ? (
-    //               <>
-    //                 <li>
-    //                   <Link className="nav-link" to={"/addAsset"}>
-    //                     Add Asset
-    //                   </Link>
-    //                 </li>
-    //                 <li>
-    //                   <Link className="nav-link" onClick={Auth.logout}>
-    //                     Logout
-    //                   </Link>
-    //                 </li>
-    //               </>
-    //             ) : (
-    //               <>
-    //                 <li className="nav-item">
-    //                   <Link className="nav-link" to={"/sign-in"}>
-    //                     Sign in
-    //                   </Link>
-    //                 </li>
-    //                 <li className="nav-item">
-    //                   <Link className="nav-link" to={"/sign-up"}>
-    //                     Sign up
-    //                   </Link>
-    //                 </li>
-    //               </>
-    //             )}
-    //           </ul>
-    //         </div>
-    //       </div>
-    //     </nav>
-
-    //     <div className="outer">
-    //       <div className="inner">
-    //         <Switch>
-    //           <Route exact path="/" component={HomePage} />
-    //           <Route path="/sign-in" component={Login} />
-    //           <Route path="/sign-up" component={SignUp} />
-    //           <Route path="/addAsset" component={AssetForm} />
-    //         </Switch>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </Router>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="outer">
+          <div className="inner">
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+              <Route path="/addAsset" component={AssetForm} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
