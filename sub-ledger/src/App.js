66f536824a5c5942ed10011cb0fd56b320a6f72b
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-09-21 16:15:58 -0400

    added .js to file names

--- sub-ledger/src/App.js
+++ sub-ledger/src/App.js
@@ -3,10 +3,10 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Login from "./components/Login/Login";
-import SignUp from "./components/Signup/Signup";
-import HomePage from "./components/HomePage/HomePage";
-import AssetForm from "./components/AssetForm/AssetForm";
+import Login from "./components/Login/Login.js";
+import SignUp from "./components/Signup/Signup.js";
+import HomePage from "./components/HomePage/HomePage.js";
+import AssetForm from "./components/AssetForm/AssetForm.js";
 
 function App() {
   return (
