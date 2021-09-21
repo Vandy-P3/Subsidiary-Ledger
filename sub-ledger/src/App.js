bc0536b5625ce5ecad72f09f3c3b3836a84625c3
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-09-21 17:32:19 -0400

    undid navbar

--- sub-ledger/src/App.js
+++ sub-ledger/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Auth from './utils/auth'
 import Login from "./components/Login/Login.js";
 import SignUp from "./components/Signup/Signup.js";
 import HomePage from "./components/HomePage/HomePage.js";
@@ -12,7 +12,44 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar />
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/"}>
+            Depreciation Ledger
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              {Auth.loggedIn() ? (
+                <>
+                  <li>
+                    <Link className="nav-link" to={"/addAsset"}>
+                      Add Asset
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="nav-link" onClick={Auth.logout}>
+                      Logout
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-in"}>
+                      Sign in
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-up"}>
+                      Sign up
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
         <div className="outer">
           <div className="inner">
             <Switch>
