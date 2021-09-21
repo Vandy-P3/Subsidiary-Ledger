31f97f466953368dc5a74d9e625c24e57d03db86
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-09-21 15:34:02 -0400

    test

--- server/server.js
+++ server/server.js
@@ -15,6 +15,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../sub-ledger/build')));
 }
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.use(routes);
 
 db.once('open', () => {
