217a8ee47cb5cc44a8d73298fa9f2a8a3458954e
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-09-21 17:19:51 -0400

    console.log user

--- sub-ledger/src/components/HomePage/HomePage.js
+++ sub-ledger/src/components/HomePage/HomePage.js
@@ -35,6 +35,7 @@ export default function BasicTable() {
         }
 
         const user = await response.json();
+        console.log(user)
         setUserData(user);
       } catch (err) {
         console.error(err);
