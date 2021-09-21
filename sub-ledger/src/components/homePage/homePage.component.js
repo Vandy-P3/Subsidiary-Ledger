cd9cc48fbe0e06f148691fa1c6a2b8b6512ce4dc
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-09-20 20:16:42 -0400

    load asset data into table

--- sub-ledger/src/components/homePage/homePage.component.js
+++ sub-ledger/src/components/homePage/homePage.component.js
@@ -1,4 +1,9 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
+
+import {getMe, deleteAsset} from '../../utils/API'
+import Auth from '../../utils/auth';
+import {removeAssetId} from '../../utils/localStorage';
+
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,19 +13,45 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import '../../components/table.css';
 
-function createData(name, bookValue, monthPurchased, usefulLife, depreciationMethod, monthlyDepExp, accumulatedDepreciation) {
-  return { name, bookValue, monthPurchased, usefulLife, depreciationMethod, monthlyDepExp, accumulatedDepreciation };
-}
-
-const rows = [
-  createData('car', 159, 6.0, 24, 4.0, 45, 64),
-  createData('boat', 237, 9.0, 37, 4.3, 45, 64),
-  createData('plane', 262, 16.0, 24, 6.0, 45, 64),
-  createData('house', 305, 3.7, 67, 4.3, 45, 64),
-  createData('lawn', 356, 16.0, 49, 3.9, 45, 64),
-];
 
 export default function BasicTable() {
+  
+  const [userData, setUserData] = useState({});
+
+  const userDataLength = Object.keys(userData).length;
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+          return false;
+        }
+
+        const response = await getMe(token);
+
+        if (!response.ok) {
+          throw new Error('something went wrong!');
+        }
+
+        const user = await response.json();
+        setUserData(user);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getUserData();
+  }, [userDataLength])
+  
+  
+  
+  
+  
+  
+  
+  
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -30,28 +61,29 @@ export default function BasicTable() {
             <TableCell align="right">Book Value</TableCell>
             <TableCell align="right">Month Purchased</TableCell>
             <TableCell align="right">Useful Life</TableCell>
-            <TableCell align="right">Depreciation Method</TableCell>
             <TableCell align="right">Monthly Depreciation Expense</TableCell>
             <TableCell align="right">Accumulated Depreciation</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.bookValue}</TableCell>
-              <TableCell align="right">{row.monthPurchased}</TableCell>
-              <TableCell align="right">{row.usefulLife}</TableCell>
-              <TableCell align="right">{row.depreciationMethod}</TableCell>
-              <TableCell align="right">{row.monthlyDepExp}</TableCell>
-              <TableCell align="right">{row.accumulatedDepreciation}</TableCell>
-            </TableRow>
-          ))}
+          {userData.assets ? (
+            userData.assets.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.bookValue}</TableCell>
+                <TableCell align="right">{row.monthPurchased}</TableCell>
+                <TableCell align="right">{row.usefulLife}</TableCell>
+                <TableCell align="right">{row.monthlyDepreciationExpense}</TableCell>
+                <TableCell align="right">{row.accumulatedDepreciation}</TableCell>
+              </TableRow>
+            ))
+            ) : null
+          }
         </TableBody>
       </Table>
     </TableContainer>
